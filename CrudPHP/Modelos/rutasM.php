1d8f1ee45b9209942e0256dec46287625c0b258c
Beta-sebas <jsebas.95@hotmail.com>
2021-10-07 00:13:36 -0500

    MVC en PHP

---
+++ CrudPHP/Modelos/rutasM.php
@@ -0,0 +1,26 @@
+<?php
+
+    class Modelo{
+
+        static public function RutasModelo($rutas){
+
+            if ($rutas == "registrar" || $rutas == "ingreso" || $rutas == "empleados" || $rutas == "salir" ) {
+                
+                $pagina = "Vistas/modulos/".$rutas.".php";
+
+            }elseif ($rutas == "index") {
+
+                $pagina = "Vistas/modulos/ingreso.php";
+
+            }else {
+
+                $pagina = "Vistas/modulos/ingreso.php";
+            }
+
+            return $pagina;
+
+        }
+
+    }
+
+?>
